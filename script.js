91992bd087d17650ee811d3eeb6df30234592974
Biswajit Rout <114167619+biswacpcode@users.noreply.github.com>
2023-06-13 00:38:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-window.onscroll = function () {
-    window.scrollTo(0,0);
-};
+scrollTop =
+          window.pageYOffset || document.documentElement.scrollTop;
+           
+           
+ // Get the current page scroll position in the horizontal direction 
+ 
+    scrollLeft =
+      window.pageXOffset || document.documentElement.scrollLeft;
+      
+      
+     // if any scroll is attempted,
+    // set this to the previous value
+    window.onscroll = function() {
+     window.scrollTo(scrollLeft, scrollTop);
+    };
+   }
+
+
 
 function displayAll() {
     window.scrollTo(0,0);
